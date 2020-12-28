cfb7f9ef277715d991761363b373014760b513ef
Tilen Tomakic <tilen@tomakic.com>
2020-12-28 18:14:10 +0100

    Revert "Extracted dropdown to jsf."
    This reverts commit 75c343904b3faa77ac513f38c9b280a4fab91f13.

--- handlers/dropdown/app/dropdown.component.ts
+++ handlers/dropdown/app/dropdown.component.ts
@@ -1,4 +1,15 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit, Optional } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Optional,
+  ViewChild
+}                                     from '@angular/core';
 import {
   JsfLayoutPropPreferences,
   JsfLayoutPropStringPreferences,
@@ -9,15 +20,22 @@ import {
   JsfPropBuilderString,
   JsfPropLayoutBuilder,
   JsfProviderExecutorStatus
-}                                                                                                                   from '@kalmia/jsf-common-es2015';
+}                                     from '@kalmia/jsf-common-es2015';
 import {
   DropdownItem,
   HandlerDropdownBuilder
-}                                                                                                                   from '../common/dropdown.builder';
-import { AbstractPropHandlerComponent, ShowValidationMessagesDirective }                                            from '@kalmia/jsf-app';
-import { takeUntil }                                                                                                from 'rxjs/operators';
-import { DropdownMessages }                                                                                         from '../common';
-import { isEqual }                                                                                                  from 'lodash';
+}                                     from '../common/dropdown.builder';
+import {
+  AbstractPropHandlerComponent,
+  jsfDefaultScrollOptions,
+  OverlayScrollbarsService,
+  ShowValidationMessagesDirective
+}                                     from '@kalmia/jsf-app';
+import { takeUntil }                  from 'rxjs/operators';
+import { DropdownMessages }           from '../common';
+import * as OverlayScrollbars         from 'overlayscrollbars';
+import { isEqual }                    from 'lodash';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-ngx';
 
 interface DropdownPreferences {
   stepperButtons?: boolean;
@@ -27,31 +45,74 @@ interface DropdownPreferences {
 @Component({
   selector       : 'app-dropdown',
   template       : `
-      <jsf-dropdown [htmlClass]="layoutSchema?.htmlClass"
-                    [variant]="themePreferences.variant"
-                    [color]="themePreferences.color"
-                    [appearance]="themePreferences.appearance"
-                    [title]="i18n(propSchema?.title)"
-                    [notitle]="layoutSchema?.notitle"
-                    [placeholder]="i18n(layoutSchema?.placeholder || '')"
-                    [required]="propSchema.required"
-                    [disabled]="disabled"
-                    [multiple]="isArray"
-                    [id]="id"
-                    [name]="propBuilder.id"
-                    [(ngModel)]="value"
-                    [layoutBuilder]="layoutBuilder"
-                    (onClick)="handleOnClick($event)"
-                    [errorStateMatcher]="errorStateMatcher"
-                    [searchable]="handlerPreferences.searchable"
-                    [searchPlaceholderLabel]="i18n(messages.searchPlaceholder)"
-                    [searchNoEntriesFoundLabel]="i18n(messages.noResultsFound)"
-                    [stepperButtons]="handlerPreferences.stepperButtons"
-                    [iconPrevious]="iconPrevious"
-                    [iconNext]="iconNext"
-                    [items]="items"
-                    [description]="i18n(propSchema?.description)">
-      </jsf-dropdown>
+      <div class="handler-common-dropdown jsf-animatable"
+           [ngClass]="layoutSchema?.htmlClass || ''"
+           [class.jsf-handler-dropdown-variant-standard]="isVariantStandard()"
+           [class.jsf-handler-dropdown-variant-small]="isVariantSmall()">
+          <div class="dropdown"
+               [class.disabled]="disabled">
+              <mat-form-field [color]="themePreferences.color"
+                              [appearance]="themePreferences.appearance"
+                              ngClass="jsf-form-field-select"
+                              [class.jsf-mat-form-field-variant-standard]="isVariantStandard()"
+                              [class.jsf-mat-form-field-variant-small]="isVariantSmall()"
+                              jsfOutlineGapAutocorrect>
+                  <mat-label *ngIf="propSchema?.title"
+                             [attr.for]="id"
+                             [style.display]="layoutSchema?.notitle ? 'none' : ''">
+                      {{ i18n(propSchema?.title) }}
+                  </mat-label>
+                  <mat-select
+                          [placeholder]="i18n(layoutSchema?.placeholder || '')"
+                          [required]="propSchema.required"
+                          [disabled]="disabled"
+                          [multiple]="isArray"
+                          [id]="id"
+                          [name]="propBuilder.id"
+                          [(ngModel)]="value"
+                          #input="ngModel"
+                          [jsfPropValidator]="layoutBuilder"
+                          (selectionChange)="handleOnClick($event)"
+                          [errorStateMatcher]="errorStateMatcher">
+                      <mat-option *ngIf="handlerPreferences.searchable">
+                          <ngx-mat-select-search ngModel
+                                                 (ngModelChange)="search = $event"
+                                                 [placeholderLabel]="i18n(messages.searchPlaceholder)"
+                                                 [noEntriesFoundLabel]="i18n(messages.noResultsFound)">
+                          </ngx-mat-select-search>
+                      </mat-option>
+
+                      <overlay-scrollbars [options]="scrollOptions">
+                          <mat-option *ngIf="!required && !isArray" [value]="null">{{ i18n('--') }}</mat-option>
+                          <mat-option *ngFor="let item of filteredItems; trackBy: trackByFn"
+                                      [value]="item.value">
+                              {{ i18n(item.label) }}
+                          </mat-option>
+                      </overlay-scrollbars>
+                  </mat-select>
+
+                  <mat-icon matPrefix *ngIf="handlerPreferences.stepperButtons"
+                            ngClass="stepper-button decrement no-text-selection __color--{{ themePreferences.color }}">
+                      {{ iconPrevious }}
+                  </mat-icon>
+
+                  <mat-icon matSuffix *ngIf="handlerPreferences.stepperButtons"
+                            ngClass="stepper-button increment no-text-selection __color--{{ themePreferences.color }}">
+                      {{ iconNext }}
+                  </mat-icon>
+
+                  <mat-hint *ngIf="propSchema?.description">{{ i18n(propSchema?.description) }}</mat-hint>
+
+                  <mat-error *ngFor="let error of propBuilder.errors">
+                      {{ error.interpolatedMessage }}
+                  </mat-error>
+              </mat-form-field>
+
+              <div class="stepper-overlay decrement no-text-selection" *ngIf="handlerPreferences.stepperButtons"
+                   (click)="previousItem()"></div>
+              <div class="stepper-overlay increment no-text-selection" *ngIf="handlerPreferences.stepperButtons" (click)="nextItem()"></div>
+          </div>
+      </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls      : ['./dropdown.component.scss']
@@ -63,16 +124,67 @@ export class DropdownComponent extends AbstractPropHandlerComponent<JsfPropBuild
     // tslint:disable-next-line:max-line-length
     layoutBuilder: JsfPropLayoutBuilder<JsfPropBuilderString | JsfPropBuilderNumber | JsfPropBuilderInteger | JsfPropBuilderId | JsfPropBuilderArray>;
 
+  @ViewChild(OverlayScrollbarsComponent, { static: false })
+  osComponent: OverlayScrollbarsComponent;
+
+  private _search                         = void 0;
+  private searchChange: EventEmitter<any> = new EventEmitter<any>();
+
+  private _filteredItems: DropdownItem[] = [];
+
+  public readonly scrollOptions: OverlayScrollbars.Options = {
+    ...jsfDefaultScrollOptions,
+    overflowBehavior: {
+      x: 'hidden',
+      y: 'scroll'
+    },
+    resize          : 'none',
+    paddingAbsolute : true
+  };
+
   private cachedValue;
 
+  public get search(): any {
+    return this._search;
+  }
+
+  public set search(x: any) {
+    this._search = x;
+    this.searchChange.emit(x);
+  }
+
   public messages = DropdownMessages;
+
+  previousItem() {
+    const idx  = this.selectedItemIndex;
+    this.value = (idx !== undefined && idx > -1) ? this.items[Math.max(0, idx - 1)].value : this.items[0].value;
+  }
+
+  nextItem() {
+    const idx  = this.selectedItemIndex;
+    this.value = (idx !== undefined && idx > -1)
+      ? this.items[Math.min(this.items.length - 1, idx + 1)].value
+      : this.items[this.items.length - 1].value;
+  }
+
+  get selectedItem(): DropdownItem {
+    if (this.value) {
+      return this.items.find(x => x.value === this.value);
+    }
+  }
+
+  get selectedItemIndex(): number {
+    if (this.value) {
+      return this.items.findIndex(x => x.value === this.value);
+    }
+  }
 
   get required(): boolean {
     return this.propSchema.required;
   }
 
   get disabled(): boolean {
-    return this.propBuilder.disabled || this.providerPending;
+    return this.propBuilder.disabled || this.providerPending || (!this.items || this.items.length === 0);
   }
 
   get providerPending(): boolean {
@@ -81,6 +193,10 @@ export class DropdownComponent extends AbstractPropHandlerComponent<JsfPropBuild
 
   get items(): DropdownItem[] {
     return this.handlerBuilder.items;
+  }
+
+  get filteredItems(): DropdownItem[] {
+    return this._filteredItems;
   }
 
   get isArray(): boolean {
@@ -167,12 +283,15 @@ export class DropdownComponent extends AbstractPropHandlerComponent<JsfPropBuild
   }
 
   constructor(protected cdRef: ChangeDetectorRef,
-              @Optional() protected showValidation: ShowValidationMessagesDirective) {
+              @Optional() protected showValidation: ShowValidationMessagesDirective,
+              private osService: OverlayScrollbarsService) {
     super(cdRef, showValidation);
   }
 
   ngOnInit(): void {
     super.ngOnInit();
+
+    this.updateFilteredItems();
 
     this.handlerBuilder.builder.statusChange
       .pipe(takeUntil(this.ngUnsubscribe))
@@ -194,6 +313,7 @@ export class DropdownComponent extends AbstractPropHandlerComponent<JsfPropBuild
     this.handlerBuilder.itemsChanged
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe(() => {
+        this.updateFilteredItems();
         this.cdRef.detectChanges();
       });
 
@@ -201,16 +321,33 @@ export class DropdownComponent extends AbstractPropHandlerComponent<JsfPropBuild
       this.handlerBuilder.itemsProvider.statusChange
         .pipe(takeUntil(this.ngUnsubscribe))
         .subscribe(() => {
+          this.updateFilteredItems();
           this.cdRef.detectChanges();
         });
     }
+
+    this.searchChange
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => {
+        this.updateFilteredItems();
+        this.cdRef.detectChanges();
+      });
   }
 
   ngAfterViewInit() {
+    this.osService.registerOverlayScrollbarsInstance(this.osComponent?.osInstance());
   }
 
   ngOnDestroy(): void {
     super.ngOnDestroy();
+
+    this.osService.deregisterOverlayScrollbarsInstance(this.osComponent?.osInstance());
+  }
+
+  private updateFilteredItems() {
+    this._filteredItems = this.search
+      ? this.items.filter(x => x.label.toLowerCase().indexOf(this.search.toLowerCase().trim()) >= 0)
+      : this.items;
   }
 
   handleOnClick($event: any) {
@@ -219,5 +356,12 @@ export class DropdownComponent extends AbstractPropHandlerComponent<JsfPropBuild
         throw e;
       });
   }
+
+  trackByFn(index, item) {
+    return item.value;
+  }
+
+  isVariantStandard = () => this.themePreferences.variant === 'standard';
+  isVariantSmall    = () => this.themePreferences.variant === 'small';
 
 }
